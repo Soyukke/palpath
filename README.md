540612c03c8433f7854896ecf8dbf236c12f4ecd
soyukke <absoexrd@gmail.com>
2024-01-24 14:24:19 +0900

    init

---
+++ README.md
@@ -0,0 +1,28 @@
+# Generate command
+
+```sh
+cargo run -- --generate zsh > palpath
+chmod 755 palpath
+```
+
+
+# algorithm
+
+initial: a, b
+target: t
+
+combine a & b, and generate t.
+
+Generation 1.
+S = {a, b}
+a, b -> c
+
+
+Generation 2.
+S = {a, b, c}
+
+a, c -> d
+b, c -> e
+
+Generation 3.
+S = {a, b, c, d, e}
---
+++ src/data.rs
@@ -0,0 +1,113 @@
+use rand::prelude::*;
+use std::collections::HashMap;
+
+#[derive(Debug, Clone)]
+pub struct Data {
+    values: HashMap<String, i32>,
+    //data: HashMap<(&'a str, &'a str), &'a str>,
+}
+
+impl Data {
+    pub fn new(values: HashMap<String, i32>) -> Self {
+        //Self { values, data }
+        Self { values }
+    }
+
+    pub fn kv(&self, a: &str) -> (String, i32) {
+        let v_a = self.values.get(a).unwrap();
+        (a.to_string(), *v_a)
+    }
+
+    pub fn combine(&self, a: &str, b: &str) -> (String, i32) {
+        let v_a = self.values.get(a).unwrap();
+        let v_b = self.values.get(b).unwrap();
+        let v_c = ((v_a + v_b) as f64) / 2.0;
+
+        let mut closest_distance = f64::INFINITY;
+        let mut closest_key: Option<&str> = None;
+        let mut closest_value: Option<i32> = None;
+        for key in self.values.keys() {
+            let x = self.values.get(key).unwrap();
+            let distance = (*x as f64 - v_c).abs();
+            if distance < closest_distance {
+                closest_distance = distance;
+                closest_key = Some(key);
+                closest_value = Some(*x);
+            }
+        }
+        (closest_key.unwrap().to_string(), closest_value.unwrap())
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use std::collections::HashSet;
+
+    use super::*;
+
+    #[test]
+    fn a00() {
+        let names: Vec<String> = (b'a'..=b'z').map(|c| (c as char).to_string()).collect();
+        let mut data_value: HashMap<String, i32> = HashMap::new();
+        let mut rng = rand::thread_rng();
+        for name in names.iter() {
+            let random_number = rng.gen_range(0..1500);
+            data_value.insert(name.to_string(), random_number);
+        }
+        let d = Data::new(data_value);
+        println!("{:?}", d);
+        let (k, v) = d.kv("a");
+        println!("(key, value) = ({:?}, {})", k, v);
+        let (k, v) = d.kv("b");
+        println!("(key, value) = ({:?}, {})", k, v);
+        let (k, v) = d.combine("a", "b");
+        println!("(key, value) = ({:?}, {})", k, v);
+        //let mut data: HashMap<(&str, &str), &str> = HashMap::new();
+        //data.insert(("a", "b"), "c");
+        //data.insert(("a", "c"), "d");
+        //data.insert(("a", "e"), "d");
+    }
+
+    //cargo test b00 -- --nocapture
+    #[test]
+    fn b00() {
+        let mut names: Vec<String> = (b'a'..=b'z').map(|c| (c as char).to_string()).collect();
+        let mut data_value: HashMap<String, i32> = HashMap::new();
+        let mut rng = rand::thread_rng();
+        for name in names.iter() {
+            let random_number = rng.gen_range(0..1500);
+            data_value.insert(name.to_string(), random_number);
+        }
+
+        println!("{:?}", data_value);
+        let d = Data::new(data_value);
+        println!("{:?}", d);
+
+        let mut s: HashSet<String> = HashSet::new();
+        s.insert("a".to_string());
+        s.insert("b".to_string());
+
+        for g in 0..10 {
+            let s_before = s.clone();
+            let sub_s: Vec<String> = s.clone().into_iter().collect();
+            for i in 0..sub_s.len() {
+                for j in i + 1..sub_s.len() {
+                    let a = &sub_s[i];
+                    let b = &sub_s[j];
+                    let (k, _) = d.combine(a, b);
+                    s.insert(k);
+                }
+            }
+            println!("Generation {g}: {:?}", s);
+            if s_before == s {
+                println!("Generation {g} is end.");
+                break;
+            }
+        }
+
+        //let mut data: HashMap<(&str, &str), &str> = HashMap::new();
+        //data.insert(("a", "b"), "c");
+        //data.insert(("a", "c"), "d");
+        //data.insert(("a", "e"), "d");
+    }
+}
